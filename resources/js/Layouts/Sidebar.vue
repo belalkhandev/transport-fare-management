c1b4e1718ff8efede107b74170d35043ea6d486b
Belal Khan <belalkhan.dev@gmail.com>
2023-10-03 22:53:23 +0600

    added transport billing navigation

--- resources/js/Layouts/Sidebar.vue
+++ resources/js/Layouts/Sidebar.vue
@@ -26,14 +26,190 @@ const getActivePath = (routeName) => {
             <div class="sidebar-item">
                 <div class="sidebar-item-header">
                         <span class="icon">
-                            <i class="bx bx-cog"></i>
+                            <i class='bx bx-bus' ></i>
                         </span>
-                    <h4>Users & Settings</h4>
+                    <h4>Transport Bill & Payment</h4>
                 </div>
                 <div class="sidebar-item-body">
                     <ul>
                         <li :class="{ 'active': $page.url === getActivePath(route('site.settings')) }">
-                            <NavLink :href="route('site.settings')">Site Settings</NavLink>
+                            <NavLink :href="route('site.settings')">
+                                <i class='bx bx-radio-circle-marked' ></i>
+                                Generate Transport Fee
+                            </NavLink>
+                        </li>
+                        <li :class="{ 'active': $page.url === getActivePath(route('site.settings')) }">
+                            <NavLink :href="route('site.settings')">
+                                <i class='bx bx-radio-circle-marked' ></i>
+                                Transport bill lists
+                            </NavLink>
+                        </li>
+                        <li :class="{ 'active': $page.url === getActivePath(route('site.settings')) }">
+                            <NavLink :href="route('site.settings')">
+                                <i class='bx bx-radio-circle-marked' ></i>
+                                Payments
+                            </NavLink>
+                        </li>
+                        <li :class="{ 'active': $page.url === getActivePath(route('site.settings')) }">
+                            <NavLink :href="route('site.settings')">
+                                <i class='bx bx-radio-circle-marked' ></i>
+                                Student Transport fees
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="sidebar-item">
+                <div class="sidebar-item-header">
+                        <span class="icon">
+                            <i class='bx bx-user' ></i>
+                        </span>
+                    <h4>Students</h4>
+                </div>
+                <div class="sidebar-item-body">
+                    <ul>
+                        <li :class="{ 'active': $page.url === getActivePath(route('site.settings')) }">
+                            <NavLink :href="route('site.settings')">
+                                <i class='bx bx-radio-circle-marked' ></i>
+                                New Students
+                            </NavLink>
+                        </li>
+                        <li :class="{ 'active': $page.url === getActivePath(route('site.settings')) }">
+                            <NavLink :href="route('site.settings')">
+                                <i class='bx bx-radio-circle-marked' ></i>
+                                Student list
+                            </NavLink>
+                        </li>
+                        <li :class="{ 'active': $page.url === getActivePath(route('site.settings')) }">
+                            <NavLink :href="route('site.settings')">
+                                <i class='bx bx-radio-circle-marked' ></i>
+                                Student Bulk Import
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="sidebar-item">
+                <div class="sidebar-item-header">
+                        <span class="icon">
+                            <i class='bx bx-message-dots' ></i>
+                        </span>
+                    <h4>SMS Panel</h4>
+                </div>
+                <div class="sidebar-item-body">
+                    <ul>
+                        <li :class="{ 'active': $page.url === getActivePath(route('site.settings')) }">
+                            <NavLink :href="route('site.settings')">
+                                <i class='bx bx-radio-circle-marked' ></i>
+                                Send SMS
+                            </NavLink>
+                        </li>
+                        <li :class="{ 'active': $page.url === getActivePath(route('site.settings')) }">
+                            <NavLink :href="route('site.settings')">
+                                <i class='bx bx-radio-circle-marked' ></i>
+                                Send Bulk SMS
+                            </NavLink>
+                        </li>
+                        <li :class="{ 'active': $page.url === getActivePath(route('site.settings')) }">
+                            <NavLink :href="route('site.settings')">
+                                <i class='bx bx-radio-circle-marked' ></i>
+                                SMS history
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+
+            <div class="sidebar-item">
+                <div class="sidebar-item-header">
+                        <span class="icon">
+                            <i class="bx bx-cog"></i>
+                        </span>
+                    <h4>Configurations</h4>
+                </div>
+                <div class="sidebar-item-body">
+                    <ul>
+                        <li :class="{ 'active': $page.url === getActivePath(route('site.settings')) }">
+                            <NavLink :href="route('site.settings')">
+                                <i class='bx bx-radio-circle-marked' ></i>
+                                Fees setup
+                            </NavLink>
+                        </li>
+                        <li :class="{ 'active': $page.url === getActivePath(route('site.settings')) }">
+                            <NavLink :href="route('site.settings')">
+                                <i class='bx bx-radio-circle-marked' ></i>
+                                Areas
+                            </NavLink>
+                        </li>
+                        <li :class="{ 'active': $page.url === getActivePath(route('site.settings')) }">
+                            <NavLink :href="route('site.settings')">
+                                <i class='bx bx-radio-circle-marked' ></i>
+                                Academic Plans
+                            </NavLink>
+                        </li>
+                        <li :class="{ 'active': $page.url === getActivePath(route('site.settings')) }">
+                            <NavLink :href="route('site.settings')">
+                                <i class='bx bx-radio-circle-marked' ></i>
+                                Academic years
+                            </NavLink>
+                        </li>
+                        <li :class="{ 'active': $page.url === getActivePath(route('site.settings')) }">
+                            <NavLink :href="route('site.settings')">
+                                <i class='bx bx-radio-circle-marked' ></i>
+                                Academic Class
+                            </NavLink>
+                        </li>
+                        <li :class="{ 'active': $page.url === getActivePath(route('site.settings')) }">
+                            <NavLink :href="route('site.settings')">
+                                <i class='bx bx-radio-circle-marked' ></i>
+                                Academic Group
+                            </NavLink>
+                        </li>
+                        <li :class="{ 'active': $page.url === getActivePath(route('site.settings')) }">
+                            <NavLink :href="route('site.settings')">
+                                <i class='bx bx-radio-circle-marked' ></i>
+                                Academic Section
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="sidebar-item">
+                <div class="sidebar-item-header">
+                        <span class="icon">
+                            <i class='bx bx-line-chart' ></i>
+                        </span>
+                    <h4>Reports</h4>
+                </div>
+                <div class="sidebar-item-body">
+                    <ul>
+                        <li :class="{ 'active': $page.url === getActivePath(route('site.settings')) }">
+                            <NavLink :href="route('site.settings')">
+                                <i class='bx bx-radio-circle-marked' ></i>
+                                Monthly Transactions
+                            </NavLink>
+                        </li>
+                        <li :class="{ 'active': $page.url === getActivePath(route('site.settings')) }">
+                            <NavLink :href="route('site.settings')">
+                                <i class='bx bx-radio-circle-marked' ></i>
+                                All Transactions
+                            </NavLink>
+                        </li>
+                        <li :class="{ 'active': $page.url === getActivePath(route('site.settings')) }">
+                            <NavLink :href="route('site.settings')">
+                                <i class='bx bx-radio-circle-marked' ></i>
+                                Due Reports
+                            </NavLink>
+                        </li>
+                        <li :class="{ 'active': $page.url === getActivePath(route('site.settings')) }">
+                            <NavLink :href="route('site.settings')">
+                                <i class='bx bx-radio-circle-marked' ></i>
+                                SMS Reports
+                            </NavLink>
                         </li>
                     </ul>
                 </div>
